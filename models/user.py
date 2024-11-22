f4a58ad987f91fe3a0032f66d2dece8abaff5b63
mawenxin01 <149878039+mawenxin01@users.noreply.github.com>
2024-11-22 13:07:05 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,18 @@
 class User:
-    def __init__(self, user_id, username, password, role):
+    def __init__(self, user_id, username, password, role, status):
         self.user_id = user_id
         self.username = username
         self.password = password
         self.role = role
+        self.status = status
 
 class Admin(User):
     def __init__(self, user_id, username, password):
         super().__init__(user_id, username, password, "admin")
 
 class Patient(User):
-    def __init__(self, user_id, username, password, name, email, emergency_contact_email, mhwp_id):
-        super().__init__(user_id, username, password, "patient")
+    def __init__(self, user_id, username, password, name, email, emergency_contact_email, mhwp_id, status):
+        super().__init__(user_id, username, password, "patient", status)
         self.name = name
         self.email = email
         self.emergency_contact_email = emergency_contact_email

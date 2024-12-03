d67d839c93409e757fad416bee3c3a67993975ff
Clara Cen <136780628+claracenn@users.noreply.github.com>
2024-12-03 14:51:26 +0000

    Delete models/appointment.py

--- models/appointment.py
+++
@@ -1,7 +0,0 @@
-class AppointmentModel:
-    def __init__(self, patient, mhwp, date_time):
-        self.patient = patient
-        self.mhwp = mhwp
-        self.date_time = date_time
-        self.confirmed = False
-    
